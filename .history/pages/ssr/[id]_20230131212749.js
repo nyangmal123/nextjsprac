5e167861deaf6335fd18544d6df0d73a309bd5ce
MarinaNam <namma123@naver.com>
2023-02-01 15:52:28 +0900

    dbjson 주소 ssr파일에도 적용

--- .history/pages/ssr/[id]_20230131212749.js
+++ .history/pages/ssr/[id]_20230131212749.js
@@ -19,7 +19,9 @@ export async function getServerSideProps(context) {
   const { params } = context;
   const { id } = params;
 
-  const response = await fetch(`https://few-awake-tarp.glitch.me/${id}`);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
   const post = await response.json();
 
   return {
